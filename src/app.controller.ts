d328a97fafb48df88fb73e801e8e008c07f57ae8
Harry Tang <harry@maxspell.com>
2025-11-12 23:42:48 +0200

    feat: add redis cache

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Inject } from '@nestjs/common';
+import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
 import { AppService } from './app.service';
 import { UseGuards } from '@nestjs/common';
 import { ApiGuard } from './api.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private readonly appService: AppService,
+  ) {}
 
   @Get('healthz')
   getHealthz(): string {
@@ -15,6 +19,17 @@ export class AppController {
   @UseGuards(ApiGuard)
   @Post()
   async event() {
+    // redis to store processing state to avoid duplicate processing
+    // process only if no runningprocessing
+    const isProcessing = await this.cacheManager.get('isProcessing');
+    if (isProcessing) {
+      console.info('Event is already being processed');
+      return { message: 'Event is already being processed' };
+    }
+
+    console.info('Event is being processed');
+    await this.cacheManager.set('isProcessing', true);
+
     const dnsRecords = await this.appService.getDNSRecords();
     const zone = await this.appService.getZone();
     const addedTLSARecords = await this.appService.addAllTLSARecords(
@@ -28,13 +43,13 @@ export class AppController {
 
     const res = {
       message: 'Event processed successfully',
-      dnsRecords: dnsRecords,
       addedTLSARecords: addedTLSARecords,
       deletedTLSARecords: deletedTLSARecords,
     };
 
     console.info(res);
 
+    await this.cacheManager.del('isProcessing');
     return res;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigService } from '@nestjs/config';
+import KeyvRedis from '@keyv/redis';
+import Keyv from 'keyv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule, ConfigModule.forRoot()],
+  imports: [
+    HttpModule,
+    ConfigModule.forRoot(),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const keyvRedis = new KeyvRedis(
+          configService.getOrThrow<string>('REDIS_URL'),
+        );
+        const keyv = new Keyv({ store: keyvRedis });
+        return {
+          stores: [keyv],
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
